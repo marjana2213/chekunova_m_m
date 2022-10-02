a86cc4f8c778b0d593ba45135404b3eacdd911c7
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2022-10-03 00:39:42 +0300

    Create hw-05-01.cpp

---
+++ hw-05-01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+int factorial(int n) {
+    int f = 1;
+    int i = 0;
+    for (i = 1; i <= n; ++i) {
+        f = f * i;
+    }
+    return f;
+}
+
+int main() {
+    double s = 0;
+    double step = 0.1;
+    double a = -1;
+    double b = 1;
+    double y = 0;
+    double e = 0.001;
+    for (double x = a; x <= b; x += 0.1) {
+        int i = 0;
+        y = cos(x);
+        s = 0;
+        do {
+            s += pow(-1, i) * (pow(x,2 * i) / factorial(2 * i));
+            i++;
+        } while (abs(y - s) >= 0.001);
+        std::cout << x << " " << s << " " << y << std::endl;
+    }
+}
