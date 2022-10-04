8de5dc3ae1c3562ea3df1e6917cb50ecb928a5f8
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2022-10-05 00:23:36 +0300

    Update hw-05-03.cpp

--- hw-05-03.cpp
+++ hw-05-03.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 int factorial(int n) {
     int f = 1;
@@ -12,20 +13,24 @@ int factorial(int n) {
 }
 
 int main() {
+    double now = 0;
     double s = 0;
     double step = 0.1;
     double a = 0;
     double b = 1;
     double y = 0;
     double e = 0.0001;
+    std::cout <<"  x    s(x)   f(x)" << std::endl;
     for (double x = a; x <= b; x += step) {
         int i = 0;
-        y = pow(M_E,2 * x);
+        y = pow(M_E, 2 * x);
         s = 0;
         do {
-            s += (pow(2 * x, i))/(factorial(i));
+            now = (pow(2 * x, i)) / (factorial(i));
+            s += now;
             i++;
-        } while (abs(y - s) >= e);
+        } while (abs(now) >= e);
+        std::cout << std::fixed << std::setprecision(4);
         std::cout << x << " " << s << " " << y << std::endl;
     }
 }
