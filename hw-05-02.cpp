9600adde7a676e4cba0d8fdc95dbfa729409803c
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2022-10-05 00:19:45 +0300

    Update hw-05-02.cpp

--- hw-05-02.cpp
+++ hw-05-02.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 int main() {
+    double now = 0;
     const double PI = acos(-1.0);
     double s = 0;
     double step = 0.05;
@@ -9,14 +11,17 @@ int main() {
     double b = 1;
     double y = 0;
     double e = 0.001;
+    std::cout << "  x     s(x)    f(x)" << std::endl;
     for (double x = a; x <= b; x += step) {
         int i = 1;
-        y = (x * sin(PI/4))/(1-2*x*cos(PI/4));
+        y = (x * sin(PI / 4)) / (1 - 2 * x * cos(PI / 4));
         s = 0;
         do {
-            s += pow(x,i) * sin(i * PI / 4);
+            now = pow(x, i) * sin(i * PI / 4);
+            s += now;
             i++;
-        } while (abs(y - s) >= e);
+        } while (abs(now) >= e);
+        std::cout << std::fixed << std::setprecision(4);
         std::cout << x << " " << s << " " << y << std::endl;
     }
 }
