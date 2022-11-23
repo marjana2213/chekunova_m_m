cb473ac205e74f1ee4d2ca19b4c1eeb5c1b17a09
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2022-11-23 21:39:16 +0300

    Update 1744d.cpp

--- chekunova_m_m/1744d.cpp
+++ chekunova_m_m/1744d.cpp
@@ -4,54 +4,48 @@
 
 int main() {
   int t = 0;
-  int n = 0;
-  int x = 0;
   std::cin >> t;
   while (t--) {
+    int n = 0;
+    int coun = 0;
+    int c_1 = 0;
     std::cin >> n;
-    std::vector<int>num;
+    std::vector<int> v1(n);
     for (int i = 0; i < n; i++) {
-      std::cin >> x;
-      num.push_back(x);
-    }
-    int counter_2 = 0;
-    for (int i = 0; i < n; i++) {
-      if (num[i] % 2 == 0) {
-        while (num[i] % 2 == 0) {
-          counter_2++;
-          num[i] /= 2;
-        }
+      std::cin >> v1[i];
+      int a = v1[i];
+      while (a % 2 == 0) {
+        a = a / 2;
+        coun++;
       }
     }
-    if (counter_2 == n) {
+    std::vector<int> v2;
+    for (int i = 0; i < n; i++) {
+      c_1 = 0;
+      int k = i + 1;
+      while (k % 2 == 0) {
+        k = k / 2;
+        c_1++;
+      }
+      v2.push_back(c_1);
+    }
+    sort(v2.begin(), v2.end(), std::greater<int>());
+    if (coun >= n)
+    {
       std::cout << 0 << std::endl;
-    } else {
-      std::vector<int>vec_2;
-      for (int i = n; i >= 1; i--) {
-        int index = 0;
-        int coun_2 = 0;
-        index = i;
-        if (index % 2 == 0) {
-          while (index % 2 == 0) {
-            coun_2++;
-            index /= 2;
-          }
-          vec_2.push_back(coun_2);
-        }
+      continue;
+    }
+    c_1 = 0;
+    for (auto i = v2.begin(); i != v2.end(); i++) {
+      coun += *i;
+      c_1++;
+      if (coun >= n) {
+        std::cout << c_1 << std::endl;
+        break;
       }
-      int action = 0; //операции
-      int act_0 = -1;
-      sort(vec_2.begin(), vec_2.end());
-      for (int j = vec_2.size() - 1; j >= 0; j--) {
-        action++;
-        counter_2 += vec_2[j];
-        if (counter_2 >= n) {
-          act_0 = action;
-          break;
-        }
-      }
-      std::cout << act_0 << std::endl;
+    }
+    if (coun < n) {
+      std::cout << -1 << std::endl;
     }
   }
 }
-    
